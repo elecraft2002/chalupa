de73576a9616d61492668816dc9cde423de47331
Vojtech Suchanek <suchavo3@cvut.cz>
2023-07-27 20:19:27 +0200

    xd

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -72,7 +72,7 @@ const richTextComponents = {
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   return (
-    <AnimatePresence /* mode="wait" */>
+    <AnimatePresence mode="wait">
       {/* <motion.div
         key={router.asPath}
         initial="initialState"
@@ -96,34 +96,51 @@ export default function App({ Component, pageProps }) {
         }}
         className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-primary"
       > */}
-      <motion.img
-        key={router.asPath + "c"}
-        // initial={{
-        //   scale: 0,
-        //   transition: { duration: 0.5, delay: 0.5 },
-        // }}
+
+      <motion.div
+        key={router.asPath + "d"}
+        initial={{ opacity: 1, zIndex: 99999 }}
         animate={{
-          zIndex: [-99999, 99999, -99999],
-          opacity: [0, 1, 0],
-          transition: { duration: 1, delay: 0.5 },
+          opacity: 0,
+          zIndex: -99999,
+          transition: { duration: 0.5, delay: 1.5 },
         }}
-        // exit={{ opacity: 1 }}
-        /* variants={{
+        exit={{
+          opacity: 1,
+          zIndex: 99999,
+          transition: { duration: 0.5 /* , delay: 0.5 */ },
+        }}
+        className="lol fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-primary"
+      >
+        <motion.img
+          key={router.asPath + "c"}
+          // initial={{
+          //   scale: 0,
+          //   transition: { duration: 0.5, delay: 0.5 },
+          // }}
+          animate={{
+            zIndex: [-99999, 99999, -99999],
+            opacity: [0, 1, 0],
+            transition: { duration: 1, delay: 0 },
+          }}
+          // exit={{ opacity: 1 }}
+          /* variants={{
           initialState: { opacity: 1 },
           animateState: { opacity: 0, transition: { duration: .1, delay: 1 } },
           exitState: { opacity: 1 },
         }} */
-        // transition={{ duration: .5, delay: .5 }}
-        src={logo.src}
-        style={{
-          left: "50vw",
-          top: "50vh",
-          transform: "translate(-50%, -50%)",
-        }}
-        className="absolute max-h-40 max-w-xs"
-      />
+          // transition={{ duration: .5, delay: .5 }}
+          src={logo.src}
+          style={{
+            left: "50vw",
+            top: "50vh",
+            transform: "translate(-50%, -50%)",
+          }}
+          className="absolute max-h-40 max-w-xs"
+        />
+      </motion.div>
       {/* </motion.div> */}
-      <motion.div
+      {/* <motion.div
         key={router.asPath + "a"}
         initial="initialState"
         animate="animateState"
@@ -134,19 +151,19 @@ export default function App({ Component, pageProps }) {
           animateState: { opacity: 1, transition: { delay: 1 } },
           exitState: { opacity: 0, transition: { duration: 1 } },
         }}
+      > */}
+      <PrismicProvider
+        internalLinkComponent={Link}
+        linkResolver={linkResolver}
+        richTextComponents={richTextComponents}
       >
-        <PrismicProvider
-          internalLinkComponent={Link}
-          linkResolver={linkResolver}
-          richTextComponents={richTextComponents}
-        >
-          <ParallaxProvider>
-            <Component {...pageProps} />
-            <GoogleAnalytics trackPageViews />
-            <PrismicPreview repositoryName={repositoryName} />
-          </ParallaxProvider>
-        </PrismicProvider>
-      </motion.div>
+        <ParallaxProvider>
+          <Component {...pageProps} />
+          <GoogleAnalytics trackPageViews />
+          <PrismicPreview repositoryName={repositoryName} />
+        </ParallaxProvider>
+      </PrismicProvider>
+      {/* </motion.div> */}
     </AnimatePresence>
   );
 }
