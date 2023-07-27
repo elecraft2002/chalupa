2597383c02b6be01435a39f29b3807322be49ff3
Vojtech Suchanek <suchavo3@cvut.cz>
2023-07-27 20:42:34 +0200

    animation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,13 +3,26 @@ import { Footer } from "./Footer";
 import Background from "./Background";
 import CookieConsent from "react-cookie-consent";
 import { PrismicRichText } from "@prismicio/react";
+import { motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 export function Layout({ locales, navigation, settings, children }) {
+  const router = useRouter();
+  // key={router.asPath}
   return (
     <div className="text-secondary ">
       <Background />
       <Header locales={locales} navigation={navigation} settings={settings} />
-      <main className="mt-16">{children}</main>
+      <motion.main
+        key={router.asPath}
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -10, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+        className="mt-16"
+      >
+        {children}
+      </motion.main>
       <Footer settings={settings} navigation={navigation} />
       <CookieConsent buttonText={settings.data.cookies_button}>
         <PrismicRichText field={settings.data.cookies} />
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,7 @@ import Link from "next/link";
 import { Heading } from "@/components/Heading";
 import { ParallaxProvider } from "react-scroll-parallax";
 import { GoogleAnalytics } from "nextjs-google-analytics";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
 import logo from "@/assets/img/logo.png";
 
@@ -70,89 +70,9 @@ const richTextComponents = {
 };
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
   return (
     <AnimatePresence mode="wait">
-      {/* <motion.div
-        key={router.asPath}
-        initial="initialState"
-        animate="animateState"
-        exit="exitState"
-        variants={{
-          initialState: {
-            opacity: 1,
-            zIndex: 99999999,
-          },
-          animateState: {
-            opacity: 0,
-            zIndex: -99999999,
-            transition: { duration: 2 delay: 1  },
-          },
-          exitState: {
-            opacity: 1,
-            zIndex: 99999999,
-            transition: { duration: 0.2 },
-          },
-        }}
-        className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-primary"
-      > */}
-
-      <motion.div
-        key={router.asPath + "d"}
-        initial={{ opacity: 1, zIndex: 99999 }}
-        animate={{
-          opacity: 0,
-          zIndex: -99999,
-          transition: { duration: 0.5, delay: 1.5 },
-        }}
-        exit={{
-          opacity: 1,
-          zIndex: 99999,
-          transition: { duration: 0.5 /* , delay: 0.5 */ },
-        }}
-        className="lol fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-primary"
-      >
-        <motion.img
-          key={router.asPath + "c"}
-          // initial={{
-          //   scale: 0,
-          //   transition: { duration: 0.5, delay: 0.5 },
-          // }}
-          animate={{
-            zIndex: [-99999, 99999, -99999],
-            opacity: [0, 1, 0],
-            transition: { duration: 1, delay: 0 },
-          }}
-          // exit={{ opacity: 1 }}
-          /* variants={{
-          initialState: { opacity: 1 },
-          animateState: { opacity: 0, transition: { duration: .1, delay: 1 } },
-          exitState: { opacity: 1 },
-        }} */
-          // transition={{ duration: .5, delay: .5 }}
-          src={logo.src}
-          style={{
-            left: "50vw",
-            top: "50vh",
-            transform: "translate(-50%, -50%)",
-          }}
-          className="absolute max-h-40 max-w-xs"
-        />
-      </motion.div>
-      {/* </motion.div> */}
-      {/* <motion.div
-        key={router.asPath + "a"}
-        initial="initialState"
-        animate="animateState"
-        exit="exitState"
-        // transition={{ duration: 2 }}
-        variants={{
-          initialState: { opacity: 0, transition: { duration: 1, delay: 1.5 } },
-          animateState: { opacity: 1, transition: { delay: 1 } },
-          exitState: { opacity: 0, transition: { duration: 1 } },
-        }}
-      > */}
-      <PrismicProvider
+            <PrismicProvider
         internalLinkComponent={Link}
         linkResolver={linkResolver}
         richTextComponents={richTextComponents}
