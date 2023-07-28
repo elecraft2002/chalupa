59c7301b22734f9865112801a9273713fd027fdf
Vojtech Suchanek <suchavo3@cvut.cz>
2023-07-28 09:50:21 +0200

    Fade

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,26 +3,26 @@ import { Footer } from "./Footer";
 import Background from "./Background";
 import CookieConsent from "react-cookie-consent";
 import { PrismicRichText } from "@prismicio/react";
-import { motion } from "framer-motion";
-import { useRouter } from "next/router";
+// import { motion } from "framer-motion";
+// import { useRouter } from "next/router";
 
 export function Layout({ locales, navigation, settings, children }) {
-  const router = useRouter();
+  // const router = useRouter();
   // key={router.asPath}
   return (
     <div className="text-secondary ">
       <Background />
       <Header locales={locales} navigation={navigation} settings={settings} />
-      <motion.main
+      {/* <motion.main
         key={router.asPath}
         initial={{ x: 10, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         exit={{ x: -10, opacity: 0 }}
         transition={{ duration: 0.3 }}
         className="mt-16"
-      >
-        {children}
-      </motion.main>
+      > */}
+      <main className="mt-16">{children}</main>
+      {/* </motion.main> */}
       <Footer settings={settings} navigation={navigation} />
       <CookieConsent buttonText={settings.data.cookies_button}>
         <PrismicRichText field={settings.data.cookies} />
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,7 @@ import Link from "next/link";
 import { Heading } from "@/components/Heading";
 import { ParallaxProvider } from "react-scroll-parallax";
 import { GoogleAnalytics } from "nextjs-google-analytics";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
 import logo from "@/assets/img/logo.png";
 
@@ -70,20 +70,38 @@ const richTextComponents = {
 };
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  // console.log(router.asPath)
+  // key={router.asPath}
   return (
-    <AnimatePresence mode="wait">
-            <PrismicProvider
-        internalLinkComponent={Link}
-        linkResolver={linkResolver}
-        richTextComponents={richTextComponents}
+    <AnimatePresence /* mode="wait" */>
+      {/* <motion.img
+        key={router.asPath + "img"}
+        src={logo.src}
+        animate={{ opacity: [0, 1, 0] }}
+        transition={{ duration: 2 }}
+        className="opacity-0"
+      /> */}
+      <motion.div
+        key={router.asPath + "main"}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: .75 }}
+        // onTransitionEndCapture={()=>console.log("Ahoj")}
       >
-        <ParallaxProvider>
-          <Component {...pageProps} />
-          <GoogleAnalytics trackPageViews />
-          <PrismicPreview repositoryName={repositoryName} />
-        </ParallaxProvider>
-      </PrismicProvider>
-      {/* </motion.div> */}
+        <PrismicProvider
+          internalLinkComponent={Link}
+          linkResolver={linkResolver}
+          richTextComponents={richTextComponents}
+        >
+          <ParallaxProvider>
+            <Component {...pageProps} />
+            <GoogleAnalytics trackPageViews />
+            <PrismicPreview repositoryName={repositoryName} />
+          </ParallaxProvider>
+        </PrismicProvider>
+      </motion.div>
     </AnimatePresence>
   );
 }
