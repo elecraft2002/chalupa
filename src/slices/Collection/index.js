dc1a8b497bbdb02869a3e27b4ef30a75c08c4545
Vojtech Suchanek <suchavo3@cvut.cz>
2023-07-29 15:42:14 +0200

    sizes

--- src/slices/Collection/index.js
+++ src/slices/Collection/index.js
@@ -14,43 +14,43 @@ const Collection = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="relative my-40 flex h-[200vh] w-full flex-col items-center justify-center py-20 sm:h-[100vh]"
+      className="relative my-40 flex h-[200vh] w-full flex-col items-center justify-center py-20 sm:h-[150vh]"
     >
-      <div className="absolute top-0 h-[200vh] w-full sm:h-[100vh]">
+      <div className="absolute top-0 h-[200vh] w-full sm:h-[150vh]">
         <Fade
           delay={200}
-          className="absolute w-2/3 sm:left-1/4 sm:h-1/3 sm:w-1/4 sm:-translate-x-1/2"
+          className="absolute w-2/3 sm:-top-1/3 sm:left-1/4 sm:h-1/3 sm:w-1/4 sm:-translate-x-1/2 sm:translate-y-1/2"
         >
           <PrismicNextImage
-            field={slice.primary.image_1.Small}
-            className="h-full w-full object-contain"
+            field={slice.primary.image_1}
+            className="h-full w-full object-cover"
           />
         </Fade>
         <Fade
           delay={200}
-          className="absolute right-4 top-[10%] w-2/3 sm:right-4 sm:top-1/4 sm:h-1/3 sm:w-1/3 "
+          className="absolute right-4 top-[10%] w-2/3 sm:-top-1/4 sm:right-4 sm:h-2/3 sm:w-1/4 sm:translate-y-1/3 "
         >
           <PrismicNextImage
             field={slice.primary.image_2.Small}
-            className="h-full w-full object-contain"
+            className="h-full w-full object-cover"
           />
         </Fade>
         <Fade
           delay={200}
-          className="absolute left-4 top-[70%] w-2/3 sm:left-0 sm:top-1/2 sm:h-1/3 sm:w-1/3"
+          className="absolute left-4 top-[70%] w-2/3 sm:left-0 sm:top-1/2 sm:h-1/2 sm:w-1/3"
         >
           <PrismicNextImage
             field={slice.primary.image_3.Small}
-            className="h-full w-full object-contain"
+            className="h-full w-full object-cover"
           />
         </Fade>
         <Fade
           delay={200}
-          className="absolute bottom-0 right-0 w-2/3 sm:left-[60%] sm:h-1/3 sm:w-1/3"
+          className="absolute bottom-0 right-0 w-2/3 sm:left-[60%] sm:h-1/2 sm:w-1/3 sm:translate-y-1/4"
         >
           <PrismicNextImage
             field={slice.primary.image_4.Small}
-            className="h-full w-full object-contain"
+            className="h-full w-full object-cover"
           />
         </Fade>
       </div>
